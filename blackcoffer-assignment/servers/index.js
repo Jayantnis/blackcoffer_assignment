fa2b30248c78109e0323a9d221cc9c831951318b
Jayant Nishad <jayantnishad34@gmail.com>
2024-02-29 01:11:47 +0530

    version 1.2

--- blackcoffer-assignment/servers/index.js
+++ blackcoffer-assignment/servers/index.js
@@ -1,36 +1,63 @@
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+const apiRoutes = require('./routes/api');
+require('dotenv').config(); 
 
-// Schema
+const app = express();
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    require: true,
-  },
-  lastName: {
-    type: String,
-  },
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/blackcoffer", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-const user = mongoose.model("user", userSchema);
 
-// connect
-mongoose
-  .connect("mongodb://127.0.0.1:27017/blackcoffer")
-  .then(() => {
-    console.log("MongoDB Conected");
-  })
-  .catch((err) => console.log("Mongo Error", err));
+// Routes
+app.use('/api', apiRoutes);
 
-// const express = require("express");
-// const server = express();
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-// server.get('/demo',(request,response)=>{
+// const mongoose = require("mongoose");
 
-//     response.send('hello');
+// // Schema
+
+// const userSchema = new mongoose.Schema({
+//   firstName: {
+//     type: String,
+//     require: true,
+//   },
+//   lastName: {
+//     type: String,
+//   },
 // });
+// const user = mongoose.model("user", userSchema);
 
-// server.listen(500, () => {
+// // connect
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/blackcoffer")
+//   .then(() => {
+//     console.log("MongoDB Conected");
+//   })
+//   .catch((err) => console.log("Mongo Error", err));
 
-//   console.log("SErver Started");
-// });
-// console.log("Hello");
+// // const express = require("express");
+// // const server = express();
+
+// // server.get('/demo',(request,response)=>{
+
+// //     response.send('hello');
+// // });
+
+// // server.listen(500, () => {
+
+// //   console.log("SErver Started");
+// // });
+// // console.log("Hello");
